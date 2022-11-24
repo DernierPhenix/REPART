024dfe9df519b16c24860785065a83e909b0b9b9
DernierPhenix <jpierre.fleury75@gmail.com>
2022-11-24 10:09:02 +0100

    MAj des controleurs Accueil & Produit et des templates Ss-cat

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -6,23 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-<<<<<<<< HEAD:src/Controller/ProduitController.php
+
 class ProduitController extends AbstractController
 {
     #[Route('/produit', name: 'app_produit')]
     public function index(): Response
     {
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
-========
-class AccueilController extends AbstractController
-{
-    #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
-    {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
->>>>>>>> karim:src/Controller/AccueilController.php
-        ]);
+            'controller_name' => 'ProduitController']);
     }
 }
